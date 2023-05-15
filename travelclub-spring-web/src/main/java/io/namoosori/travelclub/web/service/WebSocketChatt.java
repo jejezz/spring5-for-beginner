4205dce17d409c25a16b1b34930fd671569ef61b
jejezz <jejezz@gmail.com>
2023-05-15 17:28:35 +0900

    add websocket chat (sample) server

--- travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/service/WebSocketChatt.java
+++ travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/service/WebSocketChatt.java
@@ -11,6 +11,13 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 백그라운드로 돌아가는 빈을 등록하기 위해 @Service 어노테이션과,
+ * WebSocket 의 연결점을 알려주는
+ * @ServerEndoint 어노테이션을
+ * 지정한 클래스를 생성합니다.
+ * @ServerEndpoint 는 WebSocket을 활성화시키는 매핑 정보를 지정합니다.
+ */
 @Service
 @ServerEndpoint(value="/chatt")
 public class WebSocketChatt {
